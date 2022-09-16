2858834d6adf298383bfe9858159a6f7dec4848f
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-16 18:56:57 +0300

    init: reactApp initialised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
 
 function App() {
+  const [value, setValue] = useState('text from input');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ClassCounter/>
+        <ClassCounter/>
+        <ClassCounter/>
     </div>
   );
 }
