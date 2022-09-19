dce7dfe802919b7026ead05bad163559391a4b33
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-19 11:26:46 +0300

    edit: add basic styles, start working with comment section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import './styles/App.css'
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
-  const [value, setValue] = useState('text from input');
+    const [value, setValue] = useState('text from input');
 
-  return (
-    <div className="App">
-        <ClassCounter/>
-        <ClassCounter/>
-        <ClassCounter/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. JavaScript</strong>
+                    <div>JS - programming language</div>
+                </div>
+                <div className="post__btns">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 class ClassCounter extends React.Component {
 
     constructor(props) {
-      super(props);
-      this.state = {
-         count: 0
-      }
-      this.increment = this.increment.bind(this);
-      this.decrement = this.decrement.bind(this);
+        super(props);
+        this.state = {
+            count: 0
+        }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
     }
 
-    increment () {
-        this.setState({count: this.state.count + 1})
+    increment() {
+        this.setState({ count: this.state.count + 1 })
     }
 
-    decrement () {
-        this.setState({count: this.state.count - 1})
+    decrement() {
+        this.setState({ count: this.state.count - 1 })
     }
 
     render() {
