8068c10b9a45f4bea4394df7bcca320bc5461d8f
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-19 11:55:29 +0300

    edit: moved posts to side func

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React, { useState } from "react";
 import './styles/App.css'
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
 
 function App() {
     const [value, setValue] = useState('text from input');
 
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. JavaScript</strong>
-                    <div>JS - programming language</div>
-                </div>
-                <div className="post__btns">
-                    <button>Delete</button>
-                </div>
-            </div>
+            <PostItem post={{id: 1, title: 'JavaScript', body: 'Comment Content'}} />
+            <PostItem post={{id: 2, title: 'JavaScript', body: 'Comment Content'}} />
+            <PostItem post={{id: 3, title: 'JavaScript', body: 'Comment Content'}} />
+            <PostItem post={{id: 4, title: 'JavaScript', body: 'Comment Content'}} />
+            <PostItem post={{id: 5, title: 'JavaScript', body: 'Comment Content'}} />
         </div>
     );
 }
