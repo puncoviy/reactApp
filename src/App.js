ecec4a82caadac44e352354b4512437802b0edfb
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-19 12:23:08 +0300

    feat: add postList rect func

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './styles/App.css'
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('text from input');
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Comment Content'},
+        {id: 2, title: 'JavaScript', body: 'Comment Content'},
+        {id: 3, title: 'JavaScript', body: 'Comment Content'}
+    ])
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', body: 'Comment Content'},
+        {id: 2, title: 'Python', body: 'Comment Content'},
+        {id: 3, title: 'Python', body: 'Comment Content'}
+    ])
 
     return (
         <div className="App">
-            <PostItem post={{id: 1, title: 'JavaScript', body: 'Comment Content'}} />
-            <PostItem post={{id: 2, title: 'JavaScript', body: 'Comment Content'}} />
-            <PostItem post={{id: 3, title: 'JavaScript', body: 'Comment Content'}} />
-            <PostItem post={{id: 4, title: 'JavaScript', body: 'Comment Content'}} />
-            <PostItem post={{id: 5, title: 'JavaScript', body: 'Comment Content'}} />
+            <PostList posts={posts} title='Posts List 1'/>
+            <PostList posts={posts2} title='Posts List 2'/>
         </div>
     );
 }
