65f1b6eda56027bfa0a11442ea05b281350ed945
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-22 15:40:02 +0300

    feat: add dynamic title, add counter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('Text in input')
+    function increment() {
+        setLikes(likes + 1)
+    }
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
     return (
         <div className="App">
-            
+            <h2>{value}</h2>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+            <h1>Likes: {likes}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     );
 }
